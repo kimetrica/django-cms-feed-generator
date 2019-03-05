2c4572096bcb57da04fefb8efb363872b7799426
JOSEPH LUKETELO <joseph.luketelo@gmail.com>
2019-03-05 14:16:07 +0300

    Update urls.py
    updated the URL files to use `url` instead of `patterns`

--- feed_generator/urls.py
+++ feed_generator/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import *
 from feed_generator.feeds import RSSFeed
+from django.conf.urls import include, url
 
-urlpatterns = patterns('',
-    (r'^rss/$', RSSFeed()),
-)
-urlpatterns += patterns('feed_generator.views',
-    (r'^get_file/', 'get_file'),
-)
+# urlpatterns = patterns('',
+#     (r'^rss/$', RSSFeed()),
+# )
+# urlpatterns += patterns('feed_generator.views',
+#     (r'^get_file/', 'get_file'),
+# )
+
+urlpatterns = [
+  url(r'^rss/$', RSSFeed(), name='RSSFeed'),
+]
