e5eb6512ac9d48a5e147eb3e3f1407359fe42d2c
JOSEPH LUKETELO <joseph.luketelo@gmail.com>
2019-03-15 12:37:39 +0300

    Update settings.py
    update the feed limit from 100 to 10

--- feed_generator/settings.py
+++ feed_generator/settings.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
-feed_limit = getattr(settings, 'FEEDGENERATOR_FEED_LIMIT', 100)
+feed_limit = getattr(settings, 'FEEDGENERATOR_FEED_LIMIT', 10)
 exclude_keyword = getattr(settings, 'FEEDGENERATOR_EXCLUDE_KEYWORD', 'No Search')
 
