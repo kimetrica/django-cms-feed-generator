0df0685f228cd9469effa89822cf3540275e1c01
JOSEPH LUKETELO <joseph.luketelo@gmail.com>
2019-03-15 14:29:11 +0300

    Update settings.py

--- feed_generator/settings.py
+++ feed_generator/settings.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
-feed_limit = getattr(settings, 'FEEDGENERATOR_FEED_LIMIT', 10)
+feed_limit = getattr(settings, 'FEEDGENERATOR_FEED_LIMIT', 20)
 exclude_keyword = getattr(settings, 'FEEDGENERATOR_EXCLUDE_KEYWORD', 'No Search')
 
